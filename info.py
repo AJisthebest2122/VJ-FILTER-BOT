5a7188657ac096078316dc555ac37cc6f2f27f65
AJisthebest2122 <anshjoshiisthebest@gmail.com>
2025-03-31 11:08:19 +0530

    Update info.py

--- info.py
+++ info.py
@@ -88,7 +88,7 @@ PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Us
 
 
 # Links
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/downloaderzonefreemirrorleech')
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/dzonefiltergroup')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/filtergroupupdates')
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'DZONEDISCUSSION') # Support Chat Link Without https:// or @
 OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/ANSHtheGENIOUS')
